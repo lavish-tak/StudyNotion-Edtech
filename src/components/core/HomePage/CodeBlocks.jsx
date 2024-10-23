4dc1747c3727fd6b91154033d08c31c1261d684d
lavish-tak <lavish1854@gmail.com>
2024-10-24 00:50:16 +0530

    CodeBlocks component updated to final

--- src/components/core/HomePage/CodeBlocks.jsx
+++ src/components/core/HomePage/CodeBlocks.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import CTAButton from './Button';
 import HighLightText from './HighLightText';
 import { FaArrowRight } from "react-icons/fa6";
-const CodeBlocks = ({position,heading,subheading,ctabtn1,ctabtn2,bgGradient,codeColor}) => {
+import { TypeAnimation } from "react-type-animation";
+
+const CodeBlocks = ({position,heading,subheading,ctabtn1,ctabtn2,codeblock,bgGradient,codeColor}) => {
     return (
     <div className={`flex ${position} my-20 justify-between gap-10` }>
       {/** */}
@@ -29,8 +31,42 @@ const CodeBlocks = ({position,heading,subheading,ctabtn1,ctabtn2,bgGradient,code
             </CTAButton>
           </div>
       </div>
-    </div>
-  )
-}
+        {/* Section 2 */}
+        <div className="h-fit code-border flex flex-row py-3 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-[100%] lg:w-[470px]">
+        {bgGradient}
+        {/* Indexing */}
+        <div className="text-center flex flex-col   w-[10%] select-none text-richblack-400 font-inter font-bold ">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+        </div>
 
-export default CodeBlocks
+        {/* Codes */}
+        <div
+          className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-1`}
+        >
+          <TypeAnimation
+            sequence={[codeblock, 1000, ""]}
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              whiteSpace: "pre-line",
+              display: "block",
+            }}
+            omitDeletionAnimation={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodeBlocks;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,7 +79,9 @@ const Home = () => {
                     active:false
                 }
               }
-              CodeBlocks
+              codeColor={"text-yellow-25"}
+              codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+              backgroundGradient={<div className="codeblock1 absolute"></div>}
             />
           </div>
         
