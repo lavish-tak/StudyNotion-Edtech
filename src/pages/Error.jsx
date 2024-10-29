6e17cd2223c13b1246d3bca8109f98da234bba2d
lavish-tak <lavish1854@gmail.com>
2024-10-29 19:20:55 +0530

    ErrorPage created

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='flex text-3xl justify-center items-center text-red'>
+       Error - 404 Not found
+    </div>
+  )
+}
+
+export default Error
